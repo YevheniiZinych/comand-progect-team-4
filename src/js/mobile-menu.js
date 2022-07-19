277629584f9dec01cea8ede722ff853038c7eb87
AndriiLendiel <lendiel9333@gmail.com>
2022-07-19 14:19:42 +0300

    hero-header
    1

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,8 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu',);
+  
 
   const toggleMenu = () => {
     const isMenuOpen =

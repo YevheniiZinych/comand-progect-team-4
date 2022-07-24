d0ef2bffa3564ac5deba0b6b434c34fb3543301e
ZEugenii <zinich911@gmail.com>
2022-07-24 09:44:54 +0300

    fix header
    add close function mobile menu when you choice category

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,49 +1,99 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu',);
-  
+// Меню бургер
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+const iconMenu = document.querySelector('.manu-icon');
+const menuBody = document.querySelector('.header__nav-container');
+const iconClose = document.querySelector('.menu-close');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    iconClose.classList.toggle('_active');
   });
-})();
+}
+
+if (iconClose) {
+  iconClose.addEventListener('click', function (e) {
+    document.body.classList.remove('_lock');
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+    iconClose.classList.remove('_active');
+  });
+}
+
+// Плавный скрол моб. меню //
 
 const menuLinks = document.querySelectorAll('.header__nav-link[data-goto]');
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", onMenuLinkClick);
+    menuLink.addEventListener('click', onMenuLinkClick);
   });
   function onMenuLinkClick(e) {
     const menuLink = e.target;
-    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector('header').offsetHeight;
+
+      if (menuBody.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+
       window.scrollTo({
         top: gotoBlockValue,
-        behavior: "smooth"
+        behavior: 'smooth',
       });
       e.preventDefault();
     }
   }
 }
+
+// Плавный скрол навигация //
+
+const menuNav = document.querySelectorAll(
+  '.header__nav-link-desktop[data-goto]'
+);
+if (menuNav.length > 0) {
+  menuNav.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector('header').offsetHeight;
+
+      // if (menuBody.classList.contains('_active')) {
+      //   document.body.classList.remove('_lock');
+      //   iconMenu.classList.remove('_active');
+      //   menuBody.classList.remove('_active');
+      // }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  menuBody.classList.remove('_active');
+  // openMenuBtn.setAttribute('aria-expanded', false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
